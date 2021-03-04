9257a252d0c36ef24a84084a0fcb4f6927016f5f
Ricky Aston <astonmedia@hotmail.com>
2021-03-04 20:58:48 +1000

    set up mongodb

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-// Route Files
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to Database
+connectDB();
+
+// Route Files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -20,7 +25,16 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Err: ${err.message}`.red);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
