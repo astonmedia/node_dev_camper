2b0d15aa48dc29392e0ed5b286679af1320d03ee
Ricky Aston <astonmedia@hotmail.com>
2021-03-05 20:23:31 +1000

    custom middleware and mongoose middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const errorHandler = require("./middleware/error");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -26,8 +27,9 @@ if (process.env.NODE_ENV === "development") {
 // Mount Routers
 app.use("/api/v1/bootcamps", bootcamps);
 
+// Mount error handler
+app.use(errorHandler);
 const PORT = process.env.PORT || 8000;
-
 const server = app.listen(
   PORT,
   console.log(
